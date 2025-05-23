cd88ee9fc9832d31ad7c9d42926bf06bd9e59fdf
David7537 <a00835935@tec.mx>
2025-05-22 20:10:53 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-
-#include "sum.h"
-#include "sub.h"
-#include "div.h"
-
-using std::cout;
+#include "Calculadora.h"
 
 int main() {
+    Calculadora calc;
 
-    std::cout << "sum(1, 5) = " << sum(1, 5) << '\n';
-    std::cout << "sub(5, 1) = " << sub(5, 1) << '\n';
-    std::cout << "div(9, 3) = " << divide(9, 3) << '\n';
+    std::cout << "sum(1, 5) = " << calc.sumar(1, 5) << '\n';
+    std::cout << "sub(5, 1) = " << calc.restar(5, 1) << '\n';
+    std::cout << "mul(3, 4) = " << calc.multiplicar(3, 4) << '\n';
+    std::cout << "div(9, 3) = " << calc.dividir(9, 3) << '\n';
+
     return 0;
 }
